75da7896de3755181c681788ee484d191c6a7bdc
ardaaraz <ardaaraza@gmail.com>
2021-12-18 14:46:26 +0300

    Quit game when Esc is pressed

--- src/controller.cpp
+++ src/controller.cpp
@@ -35,6 +35,10 @@ void Controller::HandleInput(bool &running, Snake &snake) const {
           ChangeDirection(snake, Snake::Direction::kRight,
                           Snake::Direction::kLeft);
           break;
+        // Quit game when "Esc" is pressed.
+        case SDLK_ESCAPE:
+          running = false;
+          break;
       }
     }
   }
